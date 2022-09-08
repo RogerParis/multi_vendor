941f39bb8a6f4a67fbe347d797d965b0dc6de1d9
Roger Paris <roger.parisvicent@gmail.com>
2022-09-08 18:02:22 +0200

    adds form fields

--- lib/views/upload_products_screen.dart
+++ lib/views/upload_products_screen.dart
@@ -14,26 +14,97 @@ class _UploadProductsScreenState extends State<UploadProductsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.3,
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  color: Colors.blueGrey,
-                  child: const Center(
-                    child: Text(
-                      'You Have not\n \nPicked any Images',
-                      style: TextStyle(
-                        fontSize: 18,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    color: Colors.blueGrey,
+                    child: const Center(
+                      child: Text(
+                        'You Have not\n \nPicked any Images',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
                       ),
                     ),
                   ),
+                  Text('Category Here')
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+                child: Divider(
+                  color: Colors.cyan,
+                  thickness: 1.4,
                 ),
-              ],
-            ),
-          ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.38,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Price',
+                      hintText: 'Add Price',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.45,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Quantity',
+                      hintText: 'Add Quantity',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: TextFormField(
+                    maxLength: 100,
+                    maxLines: 2,
+                    decoration: InputDecoration(
+                      labelText: 'Product Name',
+                      hintText: 'Enter Product Name',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: TextFormField(
+                    maxLength: 100,
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                      labelText: 'Product Description',
+                      hintText: 'Enter Product Description',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
